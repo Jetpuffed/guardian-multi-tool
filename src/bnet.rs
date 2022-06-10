3b599d22b3c6b75d71dfef320942cb12f030b681
Jetpuffed <brandon@jetpuffed.org>
2022-06-10 11:44:18 -0700

    Create structs for every entity definition

---
+++ src/bnet.rs
@@ -0,0 +1,145 @@
+struct DestinyNodeStepSummaryDefinition {}
+
+struct DestinyArtDyeChannelDefinition {}
+
+struct DestinyArtDyeReferenceDefinition {}
+
+struct DestinyPlaceDefinition {}
+
+struct DestinyActivityDefinition {}
+
+struct DestinyActivityTypeDefinition {}
+
+struct DestinyClassDefinition {}
+
+struct DestinyGenderDefinition {}
+
+struct DestinyInventoryBucketDefinition {}
+
+struct DestinyRaceDefinition {}
+
+struct DestinyTalentGridDefinition {}
+
+struct DestinyUnlockDefinition {}
+
+struct DestinySandboxPerkDefinition {}
+
+struct DestinyStatGroupDefinition {}
+
+struct DestinyProgressionMappingDefinition {}
+
+struct DestinyFactionDefinition {}
+
+struct DestinyVendorGroupDefinition {}
+
+struct DestinyRewardSourceDefinition {}
+
+struct DestinyUnlockValueDefinition {}
+
+struct DestinyRewardMappingDefinition {}
+
+struct DestinyRewardSheetDefinition {}
+
+struct DestinyItemCategoryDefinition {}
+
+struct DestinyDamageTypeDefinition {}
+
+struct DestinyActivityModeDefinition {}
+
+struct DestinyMedalTierDefinition {}
+
+struct DestinyAchievementDefinition {}
+
+struct DestinyActivityGraphDefinition {}
+
+struct DestinyActivityInteractableDefinition {}
+
+struct DestinyBondDefinition {}
+
+struct DestinyCharacterCustomizationCategoryDefinition {}
+
+struct DestinyCharacterCustomizationOptionDefinition {}
+
+struct DestinyCollectibleDefinition {}
+
+struct DestinyDestinationDefinition {}
+
+struct DestinyEntitlementOfferDefinition {}
+
+struct DestinyEquipmentSlotDefinition {}
+
+struct DestinyStatDefinition {}
+
+struct DestinyInventoryItemDefinition {}
+
+struct DestinyInventoryItemLiteDefinition {}
+
+struct DestinyItemTierTypeDefinition {}
+
+struct DestinyLocationDefinition {}
+
+struct DestinyLoreDefinition {}
+
+struct DestinyMaterialRequirementSetDefinition {}
+
+struct DestinyMetricDefinition {}
+
+struct DestinyObjectiveDefinition {}
+
+struct DestinyPlatformBucketMappingDefinition {}
+
+struct DestinyPlugSetDefinition {}
+
+struct DestinyPowerCapDefinition {}
+
+struct DestinyPresentationNodeDefinition {}
+
+struct DestinyProgressionDefinition {}
+
+struct DestinyProgressionLevelRequirementDefinition {}
+
+struct DestinyRecordDefinition {}
+
+struct DestinyRewardAdjusterPointerDefinition {}
+
+struct DestinyRewardAdjusterProgressionMapDefinition {}
+
+struct DestinyRewardItemListDefinition {}
+
+struct DestinySackRewardItemListDefinition {}
+
+struct DestinySandboxPatternDefinition {}
+
+struct DestinySeasonDefinition {}
+
+struct DestinySeasonPassDefinition {}
+
+struct DestinySocketCategoryDefinition {}
+
+struct DestinySocketTypeDefinition {}
+
+struct DestinyTraitDefinition {}
+
+struct DestinyTraitCategoryDefinition {}
+
+struct DestinyUnlockCountMappingDefinition {}
+
+struct DestinyUnlockEventDefinition {}
+
+struct DestinyUnlockExpressionMappingDefinition {}
+
+struct DestinyVendorDefinition {}
+
+struct DestinyMilestoneDefinition {}
+
+struct DestinyActivityModifierDefinition {}
+
+struct DestinyReportReasonCategoryDefinition {}
+
+struct DestinyArtifactDefinition {}
+
+struct DestinyBreakerTypeDefinition {}
+
+struct DestinyChecklistDefinition {}
+
+struct DestinyEnergyTypeDefinition {}
